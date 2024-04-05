8c30fa6bc2af3f465f7c14994b5be8f8a67c9c3e
Ollincatl Jiménez Jiménez <ollincatlj@gmail.com>
2024-04-04 18:25:33 -0600

    prueba de acceso número 5

--- src/main/java/proyecto/yollicalli/YollicalliApplication.java
+++ src/main/java/proyecto/yollicalli/YollicalliApplication.java
@@ -2,7 +2,7 @@ package proyecto.yollicalli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,14 +12,17 @@ public class YollicalliApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YollicalliApplication.class, args);
 	}//main
-	@Configuration
-	public class CorsConfig implements WebMvcConfigurer {
-	    @Override
-	    public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**")
-	                .allowedOrigins("*") // Permitir solicitudes desde cualquier origen
-	                .allowedMethods("GET", "POST", "PUT", "DELETE") // Permitir estos métodos HTTP
-	                .allowedHeaders("*"); // Permitir todos los encabezados
-	    }
-	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("https://dhyanaixchelverjanvargas.github.io/Proyecto-YolliCalli/")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*");
+			}
+		};
+	}	
 }
